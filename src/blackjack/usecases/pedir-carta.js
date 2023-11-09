473e57833085cc0e83141f76b0461adceff042b3
RadaJJC <juanjose.ce79@gmail.com>
2023-11-09 12:49:14 +0100

    First commit

---
+++ src/blackjack/usecases/pedir-carta.js
@@ -0,0 +1,15 @@
+/** Esta función me permite pedir una carta
+ * 
+ * @param {Array<String>} deck  es un arreglo de string
+ * @returns {<String>}  rotorna la carta del deck
+ */
+
+
+export const pedirCarta = ( deck) => {
+
+    if ( !deck || deck.length === 0 ) {
+        throw Error('No hay cartas en el deck');
+    }
+    const carta = deck.pop();
+    return carta;
+}
